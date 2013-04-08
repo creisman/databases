319c8667febc2a231c506d58b72f6c37ffe4185d
Conor Reisman <conorreisman@gmail.com>
2013-04-07 17:43:18 -0700

    Implemented BufferPool.

--- lab1/src/java/simpledb/BufferPool.java
+++ lab1/src/java/simpledb/BufferPool.java
@@ -1,17 +1,15 @@
 package simpledb;
 
-import java.io.*;
-
-import java.util.concurrent.ConcurrentHashMap;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * BufferPool manages the reading and writing of pages into memory from
- * disk. Access methods call into it to retrieve pages, and it fetches
- * pages from the appropriate location.
+ * BufferPool manages the reading and writing of pages into memory from disk. Access methods call into it to retrieve
+ * pages, and it fetches pages from the appropriate location.
  * <p>
- * The BufferPool is also responsible for locking;  when a transaction fetches
- * a page, BufferPool checks that the transaction has the appropriate
- * locks to read/write the page.
+ * The BufferPool is also responsible for locking; when a transaction fetches a page, BufferPool checks that the
+ * transaction has the appropriate locks to read/write the page.
  * 
  * @Threadsafe, all fields are final
  */
@@ -19,59 +17,80 @@ public class BufferPool {
     /** Bytes per page, including header. */
     public static final int PAGE_SIZE = 4096;
 
-    /** Default number of pages passed to the constructor. This is used by
-    other classes. BufferPool should use the numPages argument to the
-    constructor instead. */
+    /**
+     * Default number of pages passed to the constructor. This is used by other classes. BufferPool should use the
+     * numPages argument to the constructor instead.
+     */
     public static final int DEFAULT_PAGES = 50;
+
+    private final int maxPages;
+    private final Map<Integer, Page> pages;
 
     /**
      * Creates a BufferPool that caches up to numPages pages.
-     *
-     * @param numPages maximum number of pages in this buffer pool.
+     * 
+     * @param numPages
+     *            maximum number of pages in this buffer pool.
      */
     public BufferPool(int numPages) {
-        // some code goes here
+        maxPages = numPages;
+        pages = new HashMap<Integer, Page>(maxPages, 1f); // We know the exact size to make this...
     }
 
     /**
-     * Retrieve the specified page with the associated permissions.
-     * Will acquire a lock and may block if that lock is held by another
-     * transaction.
+     * Retrieve the specified page with the associated permissions. Will acquire a lock and may block if that lock is
+     * held by another transaction.
      * <p>
-     * The retrieved page should be looked up in the buffer pool.  If it
-     * is present, it should be returned.  If it is not present, it should
-     * be added to the buffer pool and returned.  If there is insufficient
-     * space in the buffer pool, an page should be evicted and the new page
-     * should be added in its place.
-     *
-     * @param tid the ID of the transaction requesting the page
-     * @param pid the ID of the requested page
-     * @param perm the requested permissions on the page
+     * The retrieved page should be looked up in the buffer pool. If it is present, it should be returned. If it is not
+     * present, it should be added to the buffer pool and returned. If there is insufficient space in the buffer pool,
+     * an page should be evicted and the new page should be added in its place.
+     * 
+     * @param tid
+     *            the ID of the transaction requesting the page
+     * @param pid
+     *            the ID of the requested page
+     * @param perm
+     *            the requested permissions on the page
+     * @return The page of the given requested table.
+     * @throws TransactionAbortedException
+     *             if the transaction is aborted.
+     * @throws DbException
+     *             if the database throws an exception.
      */
-    public  Page getPage(TransactionId tid, PageId pid, Permissions perm)
-        throws TransactionAbortedException, DbException {
-        // some code goes here
-        return null;
+    public Page getPage(TransactionId tid, PageId pid, Permissions perm) throws TransactionAbortedException,
+            DbException {
+        if (pages.containsKey(pid.getTableId())) {
+            return pages.get(pid.getTableId());
+        }
+        if (pages.size() >= maxPages) {
+            evictPage();
+        }
+
+        Catalog cat = Database.getCatalog();
+
+        DbFile file = cat.getDbFile(pid.getTableId());
+        return file.readPage(pid);
     }
 
     /**
-     * Releases the lock on a page.
-     * Calling this is very risky, and may result in wrong behavior. Think hard
-     * about who needs to call this and why, and why they can run the risk of
-     * calling it.
-     *
-     * @param tid the ID of the transaction requesting the unlock
-     * @param pid the ID of the page to unlock
+     * Releases the lock on a page. Calling this is very risky, and may result in wrong behavior. Think hard about who
+     * needs to call this and why, and why they can run the risk of calling it.
+     * 
+     * @param tid
+     *            the ID of the transaction requesting the unlock
+     * @param pid
+     *            the ID of the page to unlock
      */
-    public  void releasePage(TransactionId tid, PageId pid) {
+    public void releasePage(TransactionId tid, PageId pid) {
         // some code goes here
         // not necessary for lab1|lab2
     }
 
     /**
      * Release all locks associated with a given transaction.
-     *
-     * @param tid the ID of the transaction requesting the unlock
+     * 
+     * @param tid
+     *            the ID of the transaction requesting the unlock
      */
     public void transactionComplete(TransactionId tid) throws IOException {
         // some code goes here
@@ -86,61 +105,59 @@ public class BufferPool {
     }
 
     /**
-     * Commit or abort a given transaction; release all locks associated to
-     * the transaction.
-     *
-     * @param tid the ID of the transaction requesting the unlock
-     * @param commit a flag indicating whether we should commit or abort
+     * Commit or abort a given transaction; release all locks associated to the transaction.
+     * 
+     * @param tid
+     *            the ID of the transaction requesting the unlock
+     * @param commit
+     *            a flag indicating whether we should commit or abort
      */
-    public void transactionComplete(TransactionId tid, boolean commit)
-        throws IOException {
+    public void transactionComplete(TransactionId tid, boolean commit) throws IOException {
         // some code goes here
         // not necessary for lab1|lab2
     }
 
     /**
-     * Add a tuple to the specified table behalf of transaction tid.  Will
-     * acquire a write lock on the page the tuple is added to(Lock 
-     * acquisition is not needed for lab2). May block if the lock cannot 
-     * be acquired.
+     * Add a tuple to the specified table behalf of transaction tid. Will acquire a write lock on the page the tuple is
+     * added to(Lock acquisition is not needed for lab2). May block if the lock cannot be acquired.
      * 
-     * Marks any pages that were dirtied by the operation as dirty by calling
-     * their markDirty bit, and updates cached versions of any pages that have 
-     * been dirtied so that future requests see up-to-date pages. 
-     *
-     * @param tid the transaction adding the tuple
-     * @param tableId the table to add the tuple to
-     * @param t the tuple to add
+     * Marks any pages that were dirtied by the operation as dirty by calling their markDirty bit, and updates cached
+     * versions of any pages that have been dirtied so that future requests see up-to-date pages.
+     * 
+     * @param tid
+     *            the transaction adding the tuple
+     * @param tableId
+     *            the table to add the tuple to
+     * @param t
+     *            the tuple to add
      */
-    public void insertTuple(TransactionId tid, int tableId, Tuple t)
-        throws DbException, IOException, TransactionAbortedException {
+    public void insertTuple(TransactionId tid, int tableId, Tuple t) throws DbException, IOException,
+            TransactionAbortedException {
         // some code goes here
         // not necessary for lab1
     }
 
     /**
-     * Remove the specified tuple from the buffer pool.
-     * Will acquire a write lock on the page the tuple is removed from. May block if
-     * the lock cannot be acquired.
-     *
-     * Marks any pages that were dirtied by the operation as dirty by calling
-     * their markDirty bit.  Does not need to update cached versions of any pages that have 
-     * been dirtied, as it is not possible that a new page was created during the deletion
-     * (note difference from addTuple).
-     *
-     * @param tid the transaction deleting the tuple.
-     * @param t the tuple to delete
+     * Remove the specified tuple from the buffer pool. Will acquire a write lock on the page the tuple is removed from.
+     * May block if the lock cannot be acquired.
+     * 
+     * Marks any pages that were dirtied by the operation as dirty by calling their markDirty bit. Does not need to
+     * update cached versions of any pages that have been dirtied, as it is not possible that a new page was created
+     * during the deletion (note difference from addTuple).
+     * 
+     * @param tid
+     *            the transaction deleting the tuple.
+     * @param t
+     *            the tuple to delete
      */
-    public  void deleteTuple(TransactionId tid, Tuple t)
-        throws DbException, TransactionAbortedException {
+    public void deleteTuple(TransactionId tid, Tuple t) throws DbException, TransactionAbortedException {
         // some code goes here
         // not necessary for lab1
     }
 
     /**
-     * Flush all dirty pages to disk.
-     * NB: Be careful using this routine -- it writes dirty data to disk so will
-     *     break simpledb if running in NO STEAL mode.
+     * Flush all dirty pages to disk. NB: Be careful using this routine -- it writes dirty data to disk so will break
+     * simpledb if running in NO STEAL mode.
      */
     public synchronized void flushAllPages() throws IOException {
         // some code goes here
@@ -148,11 +165,10 @@ public class BufferPool {
 
     }
 
-    /** Remove the specific page id from the buffer pool.
-        Needed by the recovery manager to ensure that the
-        buffer pool doesn't keep a rolled back page in its
-        cache.
-    */
+    /**
+     * Remove the specific page id from the buffer pool. Needed by the recovery manager to ensure that the buffer pool
+     * doesn't keep a rolled back page in its cache.
+     */
     public synchronized void discardPage(PageId pid) {
         // some code goes here
         // only necessary for lab5
@@ -160,25 +176,27 @@ public class BufferPool {
 
     /**
      * Flushes a certain page to disk
-     * @param pid an ID indicating the page to flush
+     * 
+     * @param pid
+     *            an ID indicating the page to flush
      */
-    private synchronized  void flushPage(PageId pid) throws IOException {
+    private synchronized void flushPage(PageId pid) throws IOException {
         // some code goes here
         // not necessary for lab1
     }
 
-    /** Write all pages of the specified transaction to disk.
+    /**
+     * Write all pages of the specified transaction to disk.
      */
-    public synchronized  void flushPages(TransactionId tid) throws IOException {
+    public synchronized void flushPages(TransactionId tid) throws IOException {
         // some code goes here
         // not necessary for lab1|lab2
     }
 
     /**
-     * Discards a page from the buffer pool.
-     * Flushes the page to disk to ensure dirty pages are updated on disk.
+     * Discards a page from the buffer pool. Flushes the page to disk to ensure dirty pages are updated on disk.
      */
-    private synchronized  void evictPage() throws DbException {
+    private synchronized void evictPage() throws DbException {
         // some code goes here
         // not necessary for lab1
     }
