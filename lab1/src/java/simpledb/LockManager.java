06fb07bda0b7694399036f49df51b49da8c1a1a7
Conor Reisman <conorreisman@gmail.com>
2013-05-14 13:27:30 -0700

    Implemented range timeouts for my locks so they don't all timeout at the same time.

--- lab1/src/java/simpledb/LockManager.java
+++ lab1/src/java/simpledb/LockManager.java
@@ -15,24 +15,73 @@ import java.util.concurrent.locks.ReentrantLock;
  * 
  */
 public class LockManager {
-	public static final int TIMEOUT = 100;
-	
+    /**
+     * The minimum amount of time to allow before timing out.
+     */
+    public static final int TIMEOUT_MIN = 100;
+
+    /**
+     * The maximum amount of time to allow before timing out.
+     */
+    public static final int TIMEOUT_MAX = 500;
+
     private final Lock locksMutator;
     private final Map<PageId, UpgradeableReentrantReadWriteLock> locks;
 
+    /**
+     * Constructor for LockManager.
+     * 
+     * Creates a class for managing mappings between transactions and locked pages.
+     * 
+     */
     public LockManager() {
         locksMutator = new ReentrantLock();
         locks = new ConcurrentHashMap<PageId, UpgradeableReentrantReadWriteLock>();
     }
 
+    /**
+     * Returns true if the given transaction has a read or write lock on the given page.
+     * 
+     * @param tid
+     *            - the tid to look up locks for.
+     * @param pid
+     *            - the pid to look up locks for.
+     * 
+     * @return true if tid holds a read or write lock on pid.
+     */
     public boolean holdsLock(TransactionId tid, PageId pid) {
         return locks.get(pid).holdsLock(tid);
     }
 
+    /**
+     * Returns true if the given page has a current writer.
+     * 
+     * @param pid
+     *            - the pid to look at.
+     * 
+     * @return true if pid has a writer.
+     */
+    public boolean isWriteLocked(PageId pid) {
+        return locks.get(pid).isWriteLocked();
+    }
+
+    /**
+     * Obtain a lock of the given type for the given tid/pid combo.
+     * 
+     * @param tid
+     *            - the tid to get a lock for.
+     * @param pid
+     *            - the pid to get a lock on.
+     * @param perm
+     *            - the lock type to acquire.
+     * 
+     * @throws TransactionAbortedException
+     *             Thrown to prevent deadlocks.
+     */
     public void getLock(TransactionId tid, PageId pid, Permissions perm) throws TransactionAbortedException {
         locksMutator.lock();
         if (!locks.containsKey(pid)) {
-            locks.put(pid, new UpgradeableReentrantReadWriteLock(TIMEOUT));
+            locks.put(pid, new UpgradeableReentrantReadWriteLock(TIMEOUT_MIN, TIMEOUT_MAX));
         }
         locksMutator.unlock();
 
@@ -45,12 +94,26 @@ public class LockManager {
         }
     }
 
+    /**
+     * Releases all locks by the given tid on the given pid. Does not cause errors if locks not held.
+     * 
+     * @param tid
+     *            - the tid to release locks for.
+     * @param pid
+     *            - the pid to release locks for.
+     */
     public void releaseLock(TransactionId tid, PageId pid) {
         UpgradeableReentrantReadWriteLock lock = locks.get(pid);
 
         lock.releaseLocks(tid);
     }
 
+    /**
+     * Release locks on all pages held by the tid.
+     * 
+     * @param tid
+     *            - the tid to release locks for.
+     */
     public void releaseLocks(TransactionId tid) {
         for (UpgradeableReentrantReadWriteLock lock : locks.values()) {
             lock.releaseLocks(tid);
--- lab1/src/java/simpledb/UpgradeableReentrantReadWriteLock.java
+++ lab1/src/java/simpledb/UpgradeableReentrantReadWriteLock.java
@@ -4,6 +4,7 @@
 package simpledb;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
@@ -11,8 +12,8 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * A reentrant lock supporting both reads and writes, and upgrading from read to
- * write. Locking is based on transactions, not threads.
+ * A reentrant lock supporting both reads and writes, and upgrading from read to write. Locking is based on
+ * transactions, not threads.
  * 
  * This implementation will not starve writers.
  * 
@@ -20,190 +21,211 @@ import java.util.concurrent.locks.ReentrantLock;
  * 
  */
 public class UpgradeableReentrantReadWriteLock {
-	private final Lock mutex;
-	private final Condition noReaders;
-	private final Condition noWriters;
-	private final Set<TransactionId> readers;
-	private final Set<TransactionId> writers;
-	private int writersWaiting;
-	private final int timeout;
+    private final Lock mutex;
+    private final Condition noReaders;
+    private final Condition noWriters;
+    private final Set<TransactionId> readers;
+    private final Set<TransactionId> writers;
+    private int writersWaiting;
+    private final int timeoutMin;
+    private final int timeoutRange;
+    private final Random rand;
 
-	/**
-	 * Constructor for UpgradeableReentrantReadWriteLock.
-	 * 
-	 * @param timeout
-	 *            - The time, in ms, to wait for a lock before throwing a
-	 *            TransactionAbortedException. If timeout is <= 0, it will not
-	 *            time out.
-	 */
-	public UpgradeableReentrantReadWriteLock(int timeout) {
-		mutex = new ReentrantLock();
-		noReaders = mutex.newCondition();
-		noWriters = mutex.newCondition();
-		readers = new HashSet<TransactionId>();
-		writers = new HashSet<TransactionId>();
-		writersWaiting = 0;
-		this.timeout = timeout;
-	}
+    /**
+     * Constructor for UpgradeableReentrantReadWriteLock.
+     * 
+     * @param timeoutMin
+     *            - The minimum time, in ms, to wait for a lock before throwing a TransactionAbortedException. If
+     *            timeout is <= 0, it will not time out.
+     * @param timeoutMax
+     *            - The minimum time, in ms, to wait for a lock before throwing a TransactionAbortedException. If
+     *            timeout is <= 0, it will not time out.
+     * 
+     * @throws IllegalArgumentException
+     *             if timeoutMax < timeoutMin
+     */
+    public UpgradeableReentrantReadWriteLock(int timeoutMin, int timeoutMax) {
+        if (timeoutMax < timeoutMin) {
+            throw new IllegalArgumentException();
+        }
 
-	/**
-	 * Gets the read lock. Blocks if not available.
-	 * 
-	 * @param tid
-	 *            the tid to get the read lock for.
-	 * @throws TransactionAbortedException 
-	 */
-	public void getReadLock(TransactionId tid) throws TransactionAbortedException {
-		mutex.lock();
+        mutex = new ReentrantLock();
+        noReaders = mutex.newCondition();
+        noWriters = mutex.newCondition();
+        readers = new HashSet<TransactionId>();
+        writers = new HashSet<TransactionId>();
+        writersWaiting = 0;
+        this.timeoutMin = timeoutMin;
+        timeoutRange = timeoutMax - timeoutMin;
+        rand = new Random();
+    }
 
-		if (!readers.contains(tid)) {
-			// Continue only if it has the write lock or there's no writer
-			// waiting or holding the lock.
-			while (!writers.contains(tid)
-					&& (writersWaiting != 0 || writers.size() != 0)) {
-				try {
-					if (timeout <= 0) {
-						noWriters.await();
-					} else {
-						boolean ret = noWriters.await(timeout, TimeUnit.MILLISECONDS);
-						if (!ret) {
-							// I MUST release the lock.
-							mutex.unlock();
-							throw new TransactionAbortedException();
-						}
-					}
-				} catch (InterruptedException ex) {
-					mutex.unlock();
-					throw new RuntimeException();
-				}
-			}
-			readers.add(tid);
-		}
-		mutex.unlock();
-	}
+    /**
+     * Gets the read lock. Blocks if not available.
+     * 
+     * @param tid
+     *            the tid to get the read lock for.
+     * @throws TransactionAbortedException
+     */
+    public void getReadLock(TransactionId tid) throws TransactionAbortedException {
+        mutex.lock();
 
-	/**
-	 * Releases a read lock.
-	 * 
-	 * @param tid
-	 *            the tid to release the lock for.
-	 * 
-	 * @throws RuntimeException
-	 *             if the transaction does not hold the read lock.
-	 */
-	public void releaseReadLock(TransactionId tid) {
-		mutex.lock();
+        if (!readers.contains(tid)) {
+            // Continue only if it has the write lock or there's no writer
+            // waiting or holding the lock.
+            while (!writers.contains(tid) && (writersWaiting != 0 || writers.size() != 0)) {
+                try {
+                    if (timeoutMin <= 0) {
+                        noWriters.await();
+                    } else {
+                        int timeout = (timeoutRange != 0 ? rand.nextInt(timeoutRange) : 0) + timeoutMin;
+                        boolean ret = noWriters.await(timeout, TimeUnit.MILLISECONDS);
+                        if (!ret) {
+                            // I MUST release the lock.
+                            mutex.unlock();
+                            throw new TransactionAbortedException();
+                        }
+                    }
+                } catch (InterruptedException ex) {
+                    mutex.unlock();
+                    throw new RuntimeException();
+                }
+            }
+            readers.add(tid);
+        }
+        mutex.unlock();
+    }
 
-		if (!readers.contains(tid)) {
-			mutex.unlock();
-			throw new RuntimeException("The transaction did not hold a lock.");
-		}
+    /**
+     * Releases a read lock.
+     * 
+     * @param tid
+     *            the tid to release the lock for.
+     * 
+     * @throws RuntimeException
+     *             if the transaction does not hold the read lock.
+     */
+    public void releaseReadLock(TransactionId tid) {
+        mutex.lock();
 
-		// Released the final lock.
-		readers.remove(tid);
+        if (!readers.contains(tid)) {
+            mutex.unlock();
+            throw new RuntimeException("The transaction did not hold a lock.");
+        }
 
-		if (readers.size() == 1 || readers.size() == 0) {
-			// Wake up all threads in case one of them is the writer that holds
-			// the last read.
-			noReaders.signalAll();
-		}
+        // Released the final lock.
+        readers.remove(tid);
 
-		mutex.unlock();
-	}
+        if (readers.size() == 1 || readers.size() == 0) {
+            // Wake up all threads in case one of them is the writer that holds
+            // the last read.
+            noReaders.signalAll();
+        }
 
-	/**
-	 * Gets the write lock. Blocks if not available. Can be used to upgrade from
-	 * a read lock.
-	 * 
-	 * @param tid
-	 *            the tid to get the write lock for.
-	 * @throws TransactionAbortedException 
-	 */
-	public void getWriteLock(TransactionId tid) throws TransactionAbortedException {
-		mutex.lock();
+        mutex.unlock();
+    }
 
-		if (!writers.contains(tid)) {
-			writersWaiting++;
-			// Continue only if there are no writers and no readers, or the only reader is the
-			// thread requesting the write lock.
-			while (writers.size() != 0 || (readers.size() != 0
-					&& (readers.size() != 1 || !readers.contains(tid)))) {
-				try {
-					if (timeout <= 0) {
-						noReaders.await();
-					} else {
-						boolean ret = noReaders.await(timeout, TimeUnit.MILLISECONDS);
-						if (!ret) {
-							// I MUST release the lock.
-							writersWaiting--;
-							mutex.unlock();
-							throw new TransactionAbortedException();
-						}
-					}
-				} catch (InterruptedException ex) {
-					writersWaiting--;
-					mutex.unlock();
-					throw new RuntimeException();
-				}
-			}
-			writers.add(tid);
-			writersWaiting--;
-		}
+    /**
+     * Gets the write lock. Blocks if not available. Can be used to upgrade from a read lock.
+     * 
+     * @param tid
+     *            the tid to get the write lock for.
+     * @throws TransactionAbortedException
+     */
+    public void getWriteLock(TransactionId tid) throws TransactionAbortedException {
+        mutex.lock();
 
-		mutex.unlock();
-	}
+        if (!writers.contains(tid)) {
+            writersWaiting++;
+            // Continue only if there are no writers and no readers, or the only reader is the
+            // thread requesting the write lock.
+            while (writers.size() != 0 || readers.size() != 0 && (readers.size() != 1 || !readers.contains(tid))) {
+                try {
+                    if (timeoutMin <= 0) {
+                        noReaders.await();
+                    } else {
+                        int timeout = (timeoutRange != 0 ? rand.nextInt(timeoutRange) : 0) + timeoutMin;
+                        boolean ret = noReaders.await(timeout, TimeUnit.MILLISECONDS);
+                        if (!ret) {
+                            // I MUST release the lock.
+                            writersWaiting--;
+                            mutex.unlock();
+                            throw new TransactionAbortedException();
+                        }
+                    }
+                } catch (InterruptedException ex) {
+                    writersWaiting--;
+                    mutex.unlock();
+                    throw new RuntimeException();
+                }
+            }
+            writers.add(tid);
+            writersWaiting--;
+        }
 
-	/**
-	 * Releases a write lock.
-	 * 
-	 * @param tid
-	 *            the tid to release the write lock for.
-	 * 
-	 * @throws RuntimeException
-	 *             if the transaction doesn't hold the lock.
-	 */
-	public void releaseWriteLock(TransactionId tid) {
-		mutex.lock();
+        mutex.unlock();
+    }
 
-		if (!writers.contains(tid)) {
-			throw new RuntimeException("The transaction does not hold the lock");
-		}
+    /**
+     * Releases a write lock.
+     * 
+     * @param tid
+     *            the tid to release the write lock for.
+     * 
+     * @throws RuntimeException
+     *             if the transaction doesn't hold the lock.
+     */
+    public void releaseWriteLock(TransactionId tid) {
+        mutex.lock();
 
-		writers.remove(tid);
+        if (!writers.contains(tid)) {
+            throw new RuntimeException("The transaction does not hold the lock");
+        }
 
-		// Wake all writers if there are any. Must also wake readers in case
-		// there are writers.
-		noReaders.signalAll();
-		noWriters.signalAll();
+        writers.remove(tid);
 
-		mutex.unlock();
-	}
+        // Wake all writers if there are any. Must also wake readers in case
+        // there are writers.
+        noReaders.signalAll();
+        noWriters.signalAll();
 
-	/**
-	 * Returns whether the transaction has a lock.
-	 * 
-	 * @param tid
-	 *            the transaction to check for locks for.
-	 * 
-	 * @return true if the transaction has a read or write lock.
-	 */
-	public boolean holdsLock(TransactionId tid) {
-		return readers.contains(tid) || writers.contains(tid);
-	}
+        mutex.unlock();
+    }
 
-	/**
-	 * Releases any held locks for a transaction.
-	 * 
-	 * @param tid
-	 *            the tid to release locks for.
-	 */
-	public void releaseLocks(TransactionId tid) {
-		if (readers.contains(tid)) {
-			releaseReadLock(tid);
-		}
+    /**
+     * Returns whether the transaction has a lock.
+     * 
+     * @param tid
+     *            the transaction to check for locks for.
+     * 
+     * @return true if the transaction has a read or write lock.
+     */
+    public boolean holdsLock(TransactionId tid) {
+        return readers.contains(tid) || writers.contains(tid);
+    }
 
-		if (writers.contains(tid)) {
-			releaseWriteLock(tid);
-		}
-	}
+    /**
+     * Releases any held locks for a transaction.
+     * 
+     * @param tid
+     *            the tid to release locks for.
+     */
+    public void releaseLocks(TransactionId tid) {
+        if (readers.contains(tid)) {
+            releaseReadLock(tid);
+        }
+
+        if (writers.contains(tid)) {
+            releaseWriteLock(tid);
+        }
+    }
+
+    /**
+     * Returns true if there is a writer.
+     * 
+     * @return true if a writer exists.
+     */
+    public boolean isWriteLocked() {
+        return writers.size() != 0;
+    }
 }
